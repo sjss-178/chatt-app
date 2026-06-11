3b8cb8150ca5d9b0a821ce96b52f7df695a522e6
SontineniJaswanthSivaSai <jsontineni@gmaill.com>
2026-06-11 15:21:48 +0530

    cors policy modified

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatt-app-production.up.railway.app",
     credentials: true,
   })
 );
